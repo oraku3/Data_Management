8ceb73f3e0ca998bc898f06164a45d0485b0ecc7
PUN <57931077+oraku3@users.noreply.github.com>
2020-08-24 01:21:02 +0900

    Add files via upload

---
+++ Judge.py
@@ -0,0 +1,8 @@
+# Victory message added
+import User_name
+def Judge(cnt,name):
+    if cnt >= 2:
+        print(name,"won!")
+    else:
+        print(name,"lost..")
+
---
+++ User_name.py
@@ -0,0 +1,6 @@
+# User name added
+def input_user_name():
+    name = str(input("Who are you? > "))
+    print("Hello, " + name +"!")
+    return name
+
